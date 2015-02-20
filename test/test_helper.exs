cd0f7066e26da03805c126b1271951415ddc03d7
Johan Warlander <johan@snowflake.nu>
2015-02-20 08:58:22 +0100

    Check for EN_DEVELOPER_TOKEN when running tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,6 @@
-ExUnit.start()
+case System.get_env("EN_DEVELOPER_TOKEN") do
+  nil -> 
+    IO.puts("ERROR: To run tests, define environment variable EN_DEVELOPER_TOKEN")
+  _else ->
+    ExUnit.start()
+end
